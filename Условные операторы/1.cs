49efdd7b160e5f9ac46d7a93c5548870019e9fb9
Sergey Khromenkov <khromenkov00@bk.ru>
2024-10-04 14:33:28 +0200

    Initial commit

---
+++ Условные операторы/1.cs
@@ -0,0 +1,13 @@
+﻿try
+{
+    Console.WriteLine("Введите свой рост");
+    double x = double.Parse(Console.ReadLine());
+    Console.WriteLine("Введите свой вес");
+    double y = double.Parse(Console.ReadLine());
+    if (x == 100 && y <= 50) Console.WriteLine("У вас нормальный вес ничего делать не нужно");
+    else Console.WriteLine("Ты жирный иди худей");
+}
+catch
+{
+    Console.WriteLine("Введите правильные значение ");
+}
